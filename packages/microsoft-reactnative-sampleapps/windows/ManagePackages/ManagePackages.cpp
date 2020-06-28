0b4bb437ce124d9de31dcf4fbac007133d20d256
Mike Francis <mike.francis@live.com>
2020-06-28 13:08:02 -0700

    fixed ManagePackages::PackageInstalling signature

--- packages/microsoft-reactnative-sampleapps/windows/ManagePackages/ManagePackages.cpp
+++ packages/microsoft-reactnative-sampleapps/windows/ManagePackages/ManagePackages.cpp
@@ -1,16 +1,43 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include "ManagePackages.h"
-#include "ManagePackages.g.cpp"
-#include <../winrt/Windows.Security.ExchangeActiveSyncProvisioning.h>
+//#include "ManagePackages.g.cpp"
 
-//using namespace winrt::Windows::Security::ExchangeActiveSyncProvisioning;
-
-namespace winrt::RNWManagePackages::implementation
+namespace RNWManagePackages
 {
-    REACT_METHOD(DeviceModel, L"deviceModel");
-    winrt::hstring SampleComponent::DeviceModel() noexcept
-    {
-        //auto info = EasClientDeviceInformation();
-        return L"";//info.SystemProductName();
+    winrt::hstring ManagePackages::FindPackageForUser(std::wstring packageName) noexcept {
+    auto _packageManager = PackageManager();
+      Package _currentPackage({nullptr});
+      winrt::hstring name(L"");
+      std::wstring estring;
+      try {
+        _currentPackage = _packageManager.FindPackageForUser(estring, packageName);
+        if (_currentPackage) {
+          name = _currentPackage.Id().Name();
+        } else {
+          name = L"ERROR: Package Name not found. App not installed or full Package Family Name not specified.";
+        }
+      } catch (winrt::hresult_error const &ex) {
+        name = ex.message();
+      }
+      return name;
     }
+
+    void ManagePackages::PackageInstalling(PackageCatalog const &sender, PackageInstallingEventArgs const &args)
+    {
+       if (args.IsComplete()) {
+          // fire event to RN
+          //WriteToTextBox("PackageCatalog - Done installing package");
+        //WriteToTextBox(args->Package->Id->FamilyName->ToString());
+      }
+    }
+
+    void ManagePackages::HookupCatalog()
+    {
+       auto catalog = PackageCatalog::OpenForCurrentPackage();
+      catalog.PackageInstalling({this, &ManagePackages::PackageInstalling});
+    }
+
+
+
+
 }
--- packages/microsoft-reactnative-sampleapps/windows/ManagePackages/ManagePackages.h
+++ packages/microsoft-reactnative-sampleapps/windows/ManagePackages/ManagePackages.h
@@ -5,7 +5,7 @@
 #include <winrt/Windows.Management.Deployment.h>
 #include <winrt/Windows.ApplicationModel.h>
 
-using namespace winrt::Windows::Foundation;
+//using namespace winrt::Windows::Foundation;
 using namespace winrt::Windows::Management::Deployment;
 using namespace winrt::Windows::ApplicationModel;
 
@@ -15,40 +15,12 @@ namespace RNWManagePackages
     struct ManagePackages 
     {
       REACT_METHOD(FindPackageForUser, L"findPackageForUser");
-      winrt::hstring ManagePackages::FindPackageForUser(std::wstring packageName) noexcept
-        {
-            auto _packageManager = PackageManager();
-            Package _currentPackage({nullptr});
-            winrt::hstring name(L"");
-            std::wstring estring;
-            try {
-              _currentPackage = _packageManager.FindPackageForUser(estring, packageName);
-              if (_currentPackage) {
-                name = _currentPackage.Id().Name();
-              } else {
-                name = L"ERROR: Package Name not found. App not installed or full Package Family Name not specified.";
-              }
-            } catch (winrt::hresult_error const &ex) {
-              name = ex.message();
-            }
-            return name;
-        }
+      winrt::hstring ManagePackages::FindPackageForUser(std::wstring packageName) noexcept;
 
-      void ManagePackages::HookupCatalog() {
-        auto catalog = PackageCatalog::OpenForCurrentPackage();
-        m_eventToken = catalog.PackageInstalling({&ManagePackages::PackageInstalling});
-      }
+      void ManagePackages::HookupCatalog();
+      void ManagePackages::PackageInstalling(PackageCatalog const &sender, PackageInstallingEventArgs const &args);
 
-      void ManagePackages::PackageInstalling(PackageCatalog const &sender, PackageInstallingEventArgs const &args) {
-        if (args.IsComplete()) {
-            // fire event to RN
-            //WriteToTextBox("PackageCatalog - Done installing package");
-          //WriteToTextBox(args->Package->Id->FamilyName->ToString());
-        }
-      }
-
-      private:
-          winrt::event_token m_eventToken;
+    private:
 
 
     };
